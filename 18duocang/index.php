903e118ff26d138950a8c01593cea0001f37301f
vbskycn <34542650+vbskycn@users.noreply.github.com>
2023-07-31 14:37:20 +0800

    1

---
+++ 18duocang/index.php
@@ -0,0 +1,11 @@
+<?php
+ 
+//重定向浏览器 
+ 
+header("Location: 	https://zbds.ml/tvbox/18duocang.json"); 
+ 
+//跳转至网址 
+ 
+exit;
+ 
+?>
---
+++ duocang/index.php
@@ -0,0 +1,11 @@
+<?php
+ 
+//重定向浏览器 
+ 
+header("Location: 	https://zbds.ml/tvbox/duocang.json"); 
+ 
+//跳转至网址 
+ 
+exit;
+ 
+?>
